a0b08352ccfc2acf7fab2eee8f75f99257b6927e
josejeffy <josejeffy@gmail.com>
2022-11-27 04:21:26 +1100

    added html embed tag

--- md2html.js
+++ md2html.js
@@ -3,6 +3,7 @@ let generateHTML = (md) => {
     md.split("\n").forEach(l => {
         let content = l.split(" ").slice(1).join(" ")
         switch (l[0]) {
+            case '<': html += content; break
             case '>': html += `<blockquote>${content}</blockquote>`; break
             case '#': html += `<h1>${content}</h1>`; break
             case '-': html += `<li>${content}</li>`; break
